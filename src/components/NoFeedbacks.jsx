9b1974535c06c1f51a22df8257b614108d7e1cfb
Yoshi Odei <yoshiodei@yahoo.com>
2023-08-09 14:36:05 +0000

    add no feedbacks component

---
+++ src/components/NoFeedbacks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NoFeedbacks() {
+  return (
+    <div className="no-feedbacks">
+        <h3>
+            There are currently no feedbacks!
+        </h3>
+    </div>
+  )
+}
