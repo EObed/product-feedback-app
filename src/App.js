22ae3d3200b0b53b53fc8bfca8f6410726acc4e8
Yoshi Odei <yoshiodei@yahoo.com>
2023-08-09 14:33:29 +0000

    minor update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
-import React from 'react';
 import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import AddFeedbackPage from './components/AddFeedbackPage';
+import Edit from './components/Edit';
 
 function App() {
+
+  const [feedbacks, setFeedbacks] = useState([]);
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<LandingPage/>}></Route>
-          <Route path="/feedback" exact element={<AddFeedbackPage/>}></Route>
+          <Route path="/" exact element={<LandingPage feedbacks={feedbacks} setFeedbacks={setFeedbacks} />}></Route>
+          <Route path="/feedback" exact element={<AddFeedbackPage setFeedbacks={setFeedbacks} feedbacks={feedbacks} />}></Route>
+          <Route path="/edit/:id" exact element={<Edit feedbacks={feedbacks} setFeedbacks={setFeedbacks} />}></Route>
         </Routes>
       
       </BrowserRouter>
